b257d234cac44fef8bdd6560d5afab5ab57f597c
ramzihaj <ramzi2020haj@gmail.com>
2025-06-20 09:57:38 +0100

    Sauvegarde avant update Angular Material/CDK

--- ecommerce-angular/src/app/navbar/navbar.component.ts
+++ ecommerce-angular/src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
   openNavbarContent(menu:any) {
+
+  }
+
+  navigateTo(path:any) {
 
   }
 }
